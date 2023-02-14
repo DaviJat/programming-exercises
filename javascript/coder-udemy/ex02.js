aec15ea11ec0246de9aadd638b92d80757386a1d
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-02-14 11:53:09 -0300

    Create ex02.js

---
+++ javascript/coder-udemy/ex02.js
@@ -0,0 +1,13 @@
+// Write a function that receives a person's age in years
+// and returns the same age in days
+
+function convertAgeInYearsToDays(age) {
+	return age * 365;
+}
+
+// Resolution
+
+function converterIdadeEmAnosParaDias(idadeEmAnos) {
+	const diasDoAno = 365;
+	return diasDoAno * idadeEmAnos;
+}
